3d025ada1fa42527b3b6dc9aefbd8e6fde8f6ab1
cuishuyuan <cuisy0703@163.com>
2022-12-07 23:34:10 +0800

    login input and button

--- src/components/login.vue
+++ src/components/login.vue
@@ -4,8 +4,13 @@
   <div class="login">
     <h1>登录</h1>
   </div>
-  <div>
-    <el-input v-model="input">dd</el-input>
+  <div class="sss">
+    <el-input v-model="input" placeholder="请输入用户名" width="50px">dd</el-input>
+    <el-input v-model="input" placeholder="请输入密码">dd</el-input>
+    <div>
+      <el-button type="primary" >确定</el-button>
+      <el-button type="primary" plain>取消</el-button>
+    </div>
   </div>
 </div>
 </template>
@@ -29,6 +34,9 @@ h1, h2 {
 li {
   display: inline-block;
   margin: 0 10px;
+}
+sss {
+  color: black;
 }
 a {
   color: black;
