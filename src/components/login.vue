f52c8233b4d2acd6313d91fbf62cff4e1f7542c2
cuishuyuan <cuisy0703@163.com>
2022-12-10 11:58:38 +0800

    init

--- src/components/login.vue
+++ src/components/login.vue
@@ -1,54 +1,119 @@
 <template>
-  <el-form :model="numberValidateForm" ref="numberValidateForm"  class="demo-ruleForm">
-  <el-form-item
-    label="年龄"
-    prop="age"
-    :rules="[
-      { required: true, message: '年龄不能为空'},
-      { type: 'number', message: '年龄必须为数字值'}
-    ]"
-  >
-    <el-input v-model.number="numberValidateForm.age" autocomplete="off"></el-input>
-  </el-form-item>
-  <el-form-item>
-    <el-button type="primary" @click="submitForm('numberValidateForm')">提交</el-button>
-    <el-button @click="resetForm('numberValidateForm')">重置</el-button>
-  </el-form-item>
-</el-form>
+    <div class="login-container">
+      <h1>登录</h1>
+      <input type="text" placeholder="用户名" v-model="username">
+      <input type="password" placeholder="密码" v-model="password">
+      <button @click="login">登录</button>
+    </div>
 </template>
-
-<script type="text/javascript">
+<script>
 export default {
   data () {
     return {
-      numberValidateForm: {
-        age: ''
-      }
+      username: '',
+      password: ''
     }
   },
   methods: {
-    submitForm (formName) {
-      this.$refs[formName].validate((valid) => {
-        if (valid) {
-          alert('submit!')
-        } else {
-          console.log('error submit!!')
-          return false
-        }
-      })
-    },
-    resetForm (formName) {
-      this.$refs[formName].resetFields()
+    login () {
+      // 登录逻辑
     }
   }
 }
 </script>
-
 <style scoped>
-.demo-ruleForm{
-  background-color: #66ccff;
+
+.login-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  /* padding-bottom: 10%; */
+  /* padding-top: 0%; */
+  width: 100%;
+  /* background-image:url(../assets/school.png); */
+  background: linear-gradient(to right, #7f00ff, #e100ff);
+  color: #fff;
 }
-.el-input{
-  width: 30%;
+
+.login-container h1 {
+  /* color: rgb(79, 54, 54); */
+  font-size: 2rem;
+  margin-bottom: 1rem;
+}
+
+.login-container input {
+  width: 300px;
+  height: 40px;
+  padding: 0 10px;
+  margin-bottom: 1rem;
+  border: none;
+  border-radius: 5px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+}
+
+.login-container button {
+  width: 300px;
+  height: 40px;
+  padding: 0 10px;
+  margin-bottom: 1rem;
+  border: none;
+  border-radius: 5px;
+  background: #fff;
+  color: #000;
+  font-size: 1.2rem;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+}
+
+.login-container button:hover {
+  cursor: pointer;
 }
 </style>
+<!-- <template>
+  <div>
+    <h1>登录</h1>
+    <el-form>
+      <el-form-item label="用户名">
+        <el-input v-model="username" />
+      </el-form-item>
+      <el-form-item label="性别">
+        <el-input v-model="gender" />
+      </el-form-item>
+      <el-form-item label="姓名">
+        <el-input v-model="name" />
+      </el-form-item>
+      <el-form-item label="身份证">
+        <el-input v-model="id" />
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="submit">登录</el-button>
+      </el-form-item>
+    </el-form>
+  </div>
+</template>
+
+<script>
+import { Form, Input, Button } from 'element-ui'
+
+export default {
+  components: {
+    'el-form': Form,
+    'el-form-item': Form.Item,
+    'el-input': Input,
+    'el-button': Button
+  },
+  data () {
+    return {
+      username: '',
+      gender: '',
+      name: '',
+      id: ''
+    }
+  },
+  methods: {
+    submit () {
+      // 处理表单提交事件
+    }
+  }
+}
+</script> -->
