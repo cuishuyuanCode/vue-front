bea5d768ff124dbafae6d2239d59560f60b9890c
cuishuyuan <cuisy0703@163.com>
2022-12-15 22:04:36 +0800

    menu

--- src/components/menu.vue
+++ src/components/menu.vue
@@ -1,31 +1,25 @@
-<!--加入顶部导航栏，侧边导航栏-->
+<!-- 课程预约 -->
 
 <template>
   <el-container class="menu-container">
     <el-aside width="200px" style="background-color: rgb(238, 241, 246)">
       <el-menu :default-openeds="['1', '3']">
 
-        <el-submenu index="1">
-          <template slot="title"><i class="el-icon-message"></i>已选课程</template>
-          <el-menu-item-group>
-            <el-menu-item index="1-1">物理</el-menu-item>
-            <el-menu-item index="1-2">高数</el-menu-item>
-          </el-menu-item-group>
-        </el-submenu>
+        <el-menu-item index="0">
+          <i class="el-icon-user"></i>
+          个人中心
+        </el-menu-item>
 
-        <el-submenu index="2">
-          <template slot="title"><i class="el-icon-menu"></i>可选课程</template>
-          <el-menu-item-group>
-            <el-menu-item index="2-1">选项1</el-menu-item>
-            <el-menu-item index="2-2">选项2</el-menu-item>
-          </el-menu-item-group>
-        </el-submenu>
 
-        <el-submenu index="3">
-          <template slot="title"><i class="el-icon-setting"></i>我的信息</template>
+        <el-menu-item index="0">
+          <i class="el-icon-user"></i>
+          课程管理
+        </el-menu-item>
+        
+        <el-submenu index="1" style="padding-top: 30px;">
+          <template slot="title"><i class="el-icon-setting"></i>课程管理</template>
           <el-menu-item-group>
-            <el-menu-item index="3-1">选项1</el-menu-item>
-            <el-menu-item index="3-2">选项2</el-menu-item>
+            <el-menu-item index="2-1">课程列表</el-menu-item>
           </el-menu-item-group>
         </el-submenu>
 
@@ -35,7 +29,16 @@
 
     <el-container direction="vertical">
       <!--      <el-header style="text-align: right; font-size: 12px">-->
-      <el-header style="text-align: right; font-size: 12px;"></el-header>
+      <el-header style="text-align: right; font-size: 12px;">
+        <el-menu :default-openeds="['1', '3']">
+          <el-menu-item index="1">ddd</el-menu-item>
+          <!-- <el-submenu index="">
+            <el-menu-item index=""></el-menu-item>
+          </el-submenu>
+          <el-menu-item index=""></el-menu-item> -->
+        </el-menu>
+      
+      </el-header>
       <el-main>
         <el-table :data="tableData">
           <el-table-column prop="date" label="日期" width="140">
