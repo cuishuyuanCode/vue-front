da9756bdb0a4980851c3c2642ed2814ce9ff16c9
cuishuyuan <cuisy0703@163.com>
2022-12-19 22:49:40 +0800

    init

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,8 @@ import Router from 'vue-router'
 import login from '@/components/login'
 import register from '@/components/register'
 import menu from '@/components/menu'
+import home from '@/components/header'
+import sidebar from '@/components/sidebar'
 
 Vue.use(Router)
 
@@ -13,6 +15,16 @@ export default new Router({
       name: 'login',
       // redirect: '/login',
       component: login
+    },
+    {
+      path: '/home',
+      name: 'home',
+      component: home
+    },
+    {
+      path: '/sidebar',
+      name: 'sidebar',
+      component: sidebar
     },
     {
       path: '/register',
