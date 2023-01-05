6efb064005dfbeadeff73b74803355e6376be086
cuishuyuan <cuisy0703@163.com>
2023-01-05 22:19:53 +0800

    rework login

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import login from '@/components/login'
-import register from '@/components/register'
-import menu from '@/components/menu'
-import home from '@/components/header'
-import sidebar from '@/components/sidebar'
+import Login from '@/pages/login/login'
 
 Vue.use(Router)
 
@@ -13,28 +9,7 @@ export default new Router({
     {
       path: '/',
       name: 'login',
-      // redirect: '/login',
-      component: login
-    },
-    {
-      path: '/home',
-      name: 'home',
-      component: home
-    },
-    {
-      path: '/sidebar',
-      name: 'sidebar',
-      component: sidebar
-    },
-    {
-      path: '/register',
-      name: 'register',
-      component: register
-    },
-    {
-      path: '/menu',
-      name: 'menu',
-      component: menu
+      component: Login
     }
   ]
 })
